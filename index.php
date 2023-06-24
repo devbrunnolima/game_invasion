32dbf40317b2c3f1aa15e3aea6c856d672a16489
Brunno <devbrunnolima@mail.com>
2023-06-24 16:36:05 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="vendor/css/reset.css">
+    <link rel="stylesheet" href="vendor/css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    
+
+    <title>Project Skulls</title>
+</head>
+<body>
+
+<form class="login-form">
+    <div class="login__header">
+        <img src="vendor/img/nick_skrull.jpg" class="icon" alt="icon enter">
+        <h1>Secret Invasion</h1>
+    </div>
+
+    <input type="text" placeholder="Player Name" class="login__input">
+    <button type="submit" class="login__button" disabled>Enter</button>
+</form>
+    
+</body>
+<script src="./vendor/js/login.js"></script>
+</html>
